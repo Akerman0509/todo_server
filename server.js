f72233ad8b23e37d950c302743739617959fb825
HMQ <hmquan191@gmail.com>
2025-01-24 15:57:17 +0700

    update đồng bộ cho bên FE

--- server.js
+++ server.js
@@ -5,19 +5,27 @@ const { connectDB } = require("./utils/connect");
 const routes = require("./routes/index.route");
 
 const app = express();
+
+// Connect to MongoDB
 connectDB();
-// app.use(cors());
+
+// Configure CORS
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Frontend origin
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+    credentials: true, // Allow credentials like cookies, authorization headers
+  })
+);
+
+// Middleware to parse JSON and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//routes
+
+// Routes
 app.use("/api/v1", routes);
-// app.get("/", (req, res) => {
-//   res.send("Welcome to the Task Manager API");
-// });
 
-
+// Start the server
 app.listen(process.env.PORT, () => {
-    console.log(`Server is on http://localhost:${process.env.PORT}`);
-  });
-  
-
+  console.log(`Server is on http://localhost:${process.env.PORT}`);
+});
